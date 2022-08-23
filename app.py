b459a7022d466182b17d37813ea21b554dc83348
Arban Hossain <arbanhossain@gmail.com>
2022-08-23 18:48:46 +0600

    firebase integration

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
-from flask import Flask, render_template, request, Response, jsonify
+from flask import Flask, render_template, request, Response, jsonify, redirect, url_for
+
+import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+import os
+import requests
 
 app = Flask(__name__)
 
+cred = credentials.Certificate(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+routine_ref = db.collection('classes').document(u'static')
+noti_ref = db.collection('notifications')
+
 @app.route('/')
 def index():
+	routine = routine_ref.get()
+  # print(routine_ref.to_dict())
+  # print([doc.to_dict() for doc in routine])
+	noti = [doc.to_dict() for doc in noti_ref.stream()]
 	admin = 1 if request.args.get('pass') == 'arban' else 0
-	return render_template('home.html', title="Home", admin=admin)
+	return render_template('home.html', title="Home", admin=admin, routine=json.dumps(routine.to_dict()), noti=json.dumps(noti))
+
+@app.route('/update', methods=['POST'])
+def update():
+	routine = request.get_json()
+	print(routine)
+	routine_ref.set(routine)
+	return jsonify({"redirect": "/"})
+
+@app.route('/update_noti', methods=['POST'])
+def update_noti():
+	noti = request.get_json()
+	doc_ref = noti_ref.document(noti.get('_id'))
+	doc_ref.set(noti)
+	return jsonify({"redirect": "/"})
+
+@app.route('/delete_noti', methods=['POST'])
+def delete_noti():
+	noti = request.get_json()
+	doc_ref = noti_ref.document(noti.get('_id'))
+	doc_ref.delete()
+	return jsonify({"redirect": "/"})
 
 if __name__ == "__main__":
     app.run()
--- static/script.js
+++ static/script.js
@@ -1,46 +1,36 @@
-// scrape routine.json and display it in the table
-/*
-$.getJSON("routine.json", function (data) {
-		var table = $("#table-body");
-		for (var day in data) {
-				var row = $("<tr>");
-				row.append($("<th>").text(day));
-				for (var time in data[day]) {
-						// s append break after every word in the string
-						var text = data[day][time].split(" ").join("</br>");
-						row.append($("<td>").html(text));
-				}
+String.fromHtmlEntities = function (string) {
+	return (string + "").replace(/&#\d+;/gm, function (s) {
+		return String.fromCharCode(s.match(/\d+/gm)[0]);
+	})
+};
 
-				// if 2 consecutive same values, then merge them
-				var prev = row.children().eq(1).text();
-				for (var i = 2; i < row.children().length; i++) {
-						var curr = row.children().eq(i).text();
-						if (prev == curr) {
-								row.children().eq(i).remove();
-								row.children().eq(i - 1).attr("colspan", 2);
-						} else {
-								prev = curr;
-						}
-				}
-				table.append(row);
-		}
-	  
-});
-*/
+const remove_hidden_char = (s) => {
+	s = s.replace(/\\n/g, "\\n")
+		.replace(/\\'/g, "\\'")
+		.replace(/\\"/g, '\\"')
+		.replace(/\\&/g, "\\&")
+		.replace(/\\r/g, "\\r")
+		.replace(/\\t/g, "\\t")
+		.replace(/\\b/g, "\\b")
+		.replace(/\\f/g, "\\f")
+	// Remove non-printable and other non-valid JSON characters
+	s = s.replace(/[\u0000-\u0019]+/g, "");
+	return s
+}
+
 const is_admin = (admin === "1") ? true : false
 
 const DAYS = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 let routine, notifications;
 
 const setup = () => {
-	$("#add_noti").prop('disabled', !is_admin)
-	if(localStorage.getItem('classnotifications_local') === null) {
+	if (localStorage.getItem('classnotifications_local') === null) {
 		notifications = []
 		localStorage.setItem('classnotifications_local', JSON.stringify(notifications))
 	} else {
 		notifications = JSON.parse(localStorage.getItem('classnotifications_local'))
 	}
-	
+
 	if (localStorage.getItem('classroutine_local') === null) {
 		routine = {
 			"sunday": [],
@@ -70,7 +60,7 @@ const populate_table_data = () => {
 	let data_row = $('#classes')
 	data_row.empty()
 	DAYS.forEach(day => {
-		routine[day].sort((a,b) => parseInt(a.time) - parseInt(b.time))
+		routine[day].sort((a, b) => parseInt(a.time) - parseInt(b.time))
 		html = ``
 		routine[day].forEach(period => {
 			html += `<p><b>${period.time}</b> - ${period.course}  <button id="${period._id}" onclick="handle_remove_period(this.id)" ${is_admin ? '' : 'style="display: none;"'}>-</button><br></p>`
@@ -86,7 +76,7 @@ const populate_notifications_data = () => {
 	let noti_list = $('#notifications_list')
 	noti_list.empty()
 	notifications.forEach(noti => {
-		noti_list.append($('<li>').html(`<b>${(new Date(noti.time)).toDateString()}</b>: ${noti.message}`))
+		noti_list.append($('<li>').html(`<button id="${noti._id}" onclick="handle_remove_notification(this.id)" ${is_admin ? '' : 'style="display: none;"'}>-</button><b>${(new Date(noti.time)).toDateString()}</b>: ${noti.message}`))
 	})
 }
 
@@ -125,19 +115,69 @@ const handle_add_noti = () => {
 	let noti = {
 		"message": $('#noti_msg').val(),
 		"time": new Date(),
-		"_id": uid() 
+		"_id": uid()
 	}
-	notifications.push(noti)
-	localStorage.setItem('classnotifications_local', JSON.stringify(notifications))
-	populate_notifications_data()
+	update_notification(noti)
+	// notifications.push(noti)
+	// localStorage.setItem('classnotifications_local', JSON.stringify(notifications))
+	// populate_notifications_data()
 	$("#noti_modal_overlay, #noti_modal_content").removeClass("active");
+	location.reload()
 
+}
+
+const handle_remove_notification = async (id) => {
+	let res = await fetch('/delete_noti', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ _id: id })
+	})
+	let data = await res.json()
+	if (data.redirect) {
+		window.location.href = data.redirect;
+	}
+}
+
+const update_routine = async () => {
+	let res = await fetch('/update', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(routine)
+	})
+	let data = await res.json()
+	if (data.redirect) {
+		window.location.href = data.redirect;
+	}
+}
+
+const update_notification = async (noti) => {
+	let res = await fetch('/update_noti', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(noti)
+	})
+	let data = await res.json()
+	if (data.redirect) {
+		window.location.href = data.redirect;
+	}
 }
 
 //populate_table_data()
 
 document.addEventListener('DOMContentLoaded', e => {
-	setup()
+	$("#add_noti").prop('disabled', !is_admin)
+	$("#update_routine_button").prop('disabled', !is_admin)
+	routine = JSON.parse(String.fromHtmlEntities(routine_fbase))
+	notifications = JSON.parse(String.fromHtmlEntities(noti_fbase))
+	// console.log(routine_fbase)
+	// console.log(noti_fbase)
+	// setup()
 	populate_table_data()
 	populate_notifications_data()
 })
